2fdc363457707a67a918191d2068af6df5ceff55
VirtualGoat <nagarkarparth875@gmail.com>
2021-01-26 00:14:19 +0530

    SQL done

--- phase1.py
+++ phase1.py
@@ -1,32 +1,32 @@
-import tweepy
-import credentials
+import sqlite3
 
-# Part of MyStreamListener in Main.ipynb
-# Streaming With Tweepy 
-# Override tweepy.StreamListener to add logic to on_status
+conn1 = sqlite3.connect('sample.db')
+print("Opened the database successfully")
 
-auth = tweepy.OAuthHandler(credentials.consumer_key, credentials.consumer_secret)
-auth.set_access_token(credentials.access_token, credentials.access_token_secret)    
-api = tweepy.API(auth,wait_on_rate_limit=True)
-
-class MyStreamListener(tweepy.StreamListener):
-
-    def on_status(self, status):
-        # Extract info from tweets
-        id_str = status.id_str
-        created_at = status.created_at
-        user_created_at = status.user.created_at
+conn = conn1.cursor()
 
 
-    def on_error(self, status_code):
-        '''
-        Since Twitter API has rate limits, 
-        stop srcraping data as it exceed to the thresold.
-        '''
-        if status_code == 420:
-            # return False to disconnect the stream
-            return False
+# conn.execute('''CREATE TABLE SAMPLE
+#             (ID INT PRIMARY KEY NOT NULL,
+#              NAME TEXT NOT NULL
+#             );''')
 
-myStreamListener = MyStreamListener()
-myStream = tweepy.Stream(auth = api.auth, listener = myStreamListener)
-myStream.filter(languages=["en"], track = settings.TRACK_WORDS)
+# conn.execute("INSERT INTO SAMPLE (ID,NAME) \
+#       VALUES (2, 'Parth')")
+
+# conn1.commit()
+output=conn.execute("SELECT COUNT(*) FROM SAMPLE")
+
+# conn.execute("DELETE from SAMPLE where ID = 2")
+# conn.execute("DELETE from SAMPLE where ID = 1")
+
+result=output.fetchall()
+print(result)
+
+# for row in output:
+#     print("ID:", row[0])
+#     print("Name:", row[1])
+    
+
+# conn.commit()
+conn.close()
