038dec88ad8194ebf99cbda7d04ee4e797aaca4e
Parth Nagarkar <nagarkarparth875@gmail.com>
2021-02-04 01:28:00 +0530

    Delete keys.py

--- keys.py
+++
@@ -1,9 +0,0 @@
-consumer_key='cQLpKHK4YWoH9DNMePH5OEONf'
-consumer_secret= 'FQIA1D48eqALlYvRYuic6dth4gurAcMwCVd0Z8ktDmgtVt9Zec'
-access_token='1002268050513575936-nj1aWbVHQZRvVrfaY0GXYlFnKktPeM'
-access_token_secret='2e91wc6xyiTvWmHq41LTiCPPKQNjKtple69zOUsWIcXjv'
-
-classpath="D:/Parth/Downloads/Stanford NER files/stanford-ner-2015-04-20/stanford-ner.jar"
-stanford_models='D:/Parth/Downloads/Stanford NER files/stanford-ner-2015-04-20/stanford-corenlp-full-2018-10-05/stanford-corenlp-full-2018-10-05/edu/stanford/nlp/models/ner'
-java_path = "C:/Program Files/Java/jdk1.8.0_131/bin/java.exe"
-stanford_classifier='D:/Parth/Downloads/Stanford NER files/stanford-ner-2015-04-20/stanford-corenlp-full-2018-10-05/stanford-corenlp-full-2018-10-05/edu/stanford/nlp/models/ner/english.all.3class.distsim.crf.ser.gz'
