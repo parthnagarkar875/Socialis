3e84a16eddc20ff5ac99dc34fa223c13c98e647b
Parth <nagarkarparth875@gmail.com>
2021-04-01 20:46:42 +0530

    Initialize the app

---
+++ credentials.py
@@ -0,0 +1,4 @@
+consumer_key = 'Mu57qpvkoJibYYFjhNG0uqMIR'
+consumer_secret = 'iAQo0wbkLRGSpCpvM3atX0znzLOyvigPbC5IXfapgAKUIavg3K'
+access_token = '1350457320304746497-1j6W3eC0tvMFwRcu4xHdTWRwbkU1VM'
+access_token_secret = 'ibfA0Lhk5cKYadwQhs4C6IpTc04qk54nm93ldUdxlfrlH'
