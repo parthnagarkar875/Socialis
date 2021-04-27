965d13c755654f87f7592515aa43b37e8108a974
Parth Nagarkar <nagarkarparth875@gmail.com>
2021-04-28 02:11:27 +0530

    Delete credentials.py

--- credentials.py
+++
@@ -1,8 +0,0 @@
-consumer_key = 'Mu57qpvkoJibYYFjhNG0uqMIR'
-consumer_secret = 'iAQo0wbkLRGSpCpvM3atX0znzLOyvigPbC5IXfapgAKUIavg3K'
-access_token = '1350457320304746497-1j6W3eC0tvMFwRcu4xHdTWRwbkU1VM'
-access_token_secret = 'ibfA0Lhk5cKYadwQhs4C6IpTc04qk54nm93ldUdxlfrlH'
-dbname = 'test'
-user = 'postgres'
-host = 'localhost'
-password = 'helloParth'
